2ca64c62b91c75135df4c2c520ccad395294fb64
Bamisile Tolulope <tolusnehhil@gmail.com>
2022-07-11 15:38:55 +0100

    chg

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,38 +1,3 @@
-//document.getElementById("counter").innerText = 10;
-// let myAge = 22;
-// let humanDogRatio = 7;
-// myAge=7
-// let myDogAge = myAge * humanDogRatio;
-
-// console.log(myDogAge);
-
-// let bonusPoint = 50;
-// console.log(bonusPoint);
-// bonusPoint += 50;
-// console.log(bonusPoint);
-// bonusPoint -= 75;
-// console.log(bonusPoint);
-// bonusPoint += 45;
-// console.log(bonusPoint);
-
-// let inc = 34;
-// let incr = 33;
-// let incre = 36;
-
-// function rt() {
-//     console.log(inc + incr + incre);
-// }
-// rt();
-
-// let cent = 0;
-// function yet() {
-//     cent += 1 
-// }
-// yet();
-// yet();
-// yet();
-// console.log(cent);
-
 // let increment = 0;
 // let counters =  document.getElementById("counter");
 // function count() {
@@ -44,7 +9,3 @@
 // //     console.log(increment);
 // // };
  
-let username = "Tolu";
-let message = "Hi my name is ";
-var myGreeting = message + username + "!" ;
-console.log(myGreeting);
--- tutorial/welcome.js
+++ tutorial/welcome.js
@@ -6,3 +6,43 @@ let render = greeting + name
 welcomeEl.innerText = render;
 
   welcomeEl.innerText += "🆕";
+
+let username = "Tolu";
+let message = "Hi my name is ";
+var myGreeting = message + username + "!" ;
+console.log(myGreeting);
+
+document.getElementById("counter").innerText = 10;
+let myAge = 22;
+let humanDogRatio = 7;
+myAge=7
+let myDogAge = myAge * humanDogRatio;
+
+console.log(myDogAge);
+
+let bonusPoint = 50;
+console.log(bonusPoint);
+bonusPoint += 50;
+console.log(bonusPoint);
+bonusPoint -= 75;
+console.log(bonusPoint);
+bonusPoint += 45;
+console.log(bonusPoint);
+
+let inc = 34;
+let incr = 33;
+let incre = 36;
+
+function rt() {
+    console.log(inc + incr + incre);
+}
+rt();
+
+let cent = 0;
+function yet() {
+    cent += 1 
+}
+yet();
+yet();
+yet();
+console.log(cent);
